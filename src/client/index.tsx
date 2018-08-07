dc81489acdad3cc5ce6fe9e167aa244be7cc57e2
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-07 17:57:18 +0200

    Import redux store in application and load token from localstorage

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,4 +2,23 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import App from './app';
 
-render(<App />, document.getElementById('app'));
+import { Provider } from 'react-redux';
+import store from './store';
+
+import * as jwt from 'jsonwebtoken';
+import setAuthorizationToken from './utils/setAuthorizationToken';
+import { setCurrentUser } from './services/auth/auth.actions';
+
+const token = localStorage.getItem('jwtToken');
+
+if(token) {
+  setAuthorizationToken(token);
+  store.dispatch(setCurrentUser(jwt.decode(token)));
+}
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('app')
+);
