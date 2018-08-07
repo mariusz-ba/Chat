c472693eab064ee3b77180f3eae36eefb1ec441b
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-07 22:44:52 +0200

    Connect to database and create errors handling middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,7 +2,8 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as path from 'path';
-import { Router } from './controllers';
+import * as mongoose from 'mongoose';
+import { Router } from './modules/module';
 
 class App {
   
@@ -13,6 +14,7 @@ class App {
     this.app = express();
     this.config();
     this.router.routes(this.app);
+    this.handleErrors();
   }
 
   private config(): void {
@@ -24,8 +26,23 @@ class App {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(express.static(path.join(__dirname, '../../dist')));
-  }
 
+    // Configure database connection
+    mongoose.connect('mongodb://127.0.0.1:27017/chat', {
+      useNewUrlParser: true
+    })
+  }
+  
+  private handleErrors() {
+    this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      const result = { error: err.message };
+
+      if(this.app.get('env') === 'production')
+        result.error = 'Unable to handle the request';
+
+      res.status(500).json(result);
+    })
+  }
 }
 
 export default new App().app;
--- src/server/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from 'express';
-
-export class Router {
-  public routes(app: Application): void {
-    // Configure controllers
-  }
-}
