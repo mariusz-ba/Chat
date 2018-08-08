7ec29418c171048130b98de9ab0bb076ce4689c9
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-08 19:43:48 +0200

    Create sign in/up pages

--- src/client/screens/authentication/signin/signin.tsx
+++ src/client/screens/authentication/signin/signin.tsx
@@ -1,7 +1,81 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { ICredentials, signIn } from '../../../services/auth/auth.actions';
+import { IState as IAuthState } from '../../../services/auth/auth.constants';
 
-export default () => (
-  <div>
-    <h1>Signin</h1>
-  </div>
-)
+import styled from 'styled-components';
+
+import Button from '../../../components/button/button';
+import Card from '../components/card/card';
+import Form from '../components/form/form';
+import auth from '../../../services/auth';
+
+const Layout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #9933ff;
+`
+
+interface IProps {
+  auth: IAuthState,
+  signIn(credentials: ICredentials): any
+}
+
+interface IState {
+  identifier: string,
+  password: string
+}
+
+export class Signin extends React.Component<IProps, IState> {
+  state = {
+    identifier: '',
+    password: ''
+  }
+
+  handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.name as keyof IState;
+    const value = e.currentTarget.value as string;
+    const data: any = {};
+    data[name] = value;
+    this.setState(data);
+  }
+
+  handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { identifier, password } = this.state;
+    this.props.signIn({ identifier, password });
+  }
+
+  render() {
+    const { identifier, password } = this.state;
+    const { errors } = this.props.auth;
+
+    return (
+      <Layout>
+        <Card>
+          <Card.Title text="Sign in"/>
+          { errors &&
+            <Card.Error message={errors.form}/>
+          }
+          <Form>
+            <Form.Group error={errors && errors.form}>
+              <label htmlFor="identifier">Username or E-Mail</label>
+              <input id="identifier" type="text" name="identifier" value={identifier} onChange={this.handleChange}/>
+            </Form.Group>
+            <Form.Group error={errors && errors.form}>
+              <label htmlFor="password">Password</label>
+              <input id="password" type="password" name="password" value={password} onChange={this.handleChange}/>
+            </Form.Group>
+            <Button mode="primary" onClick={this.handleSubmit}>Sign in</Button>
+          </Form>
+        </Card>
+      </Layout>
+    )
+  }
+}
+
+const mapStateToProps = ({ auth }: { auth: any }) => ({ auth });
+
+export default connect(mapStateToProps, { signIn })(Signin);
--- src/client/screens/authentication/signup/signup.tsx
+++ src/client/screens/authentication/signup/signup.tsx
@@ -1,7 +1,116 @@
 import * as React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
-export default () => (
-  <div>
-    <h1>Signup</h1>
-  </div>
-)
+import Button from '../../../components/button/button';
+import Card from '../components/card/card';
+import Form from '../components/form/form';
+
+const Layout = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #9933ff;
+`
+
+interface IError {
+  username: string,
+  email: string,
+  confirm: string
+}
+
+interface IState {
+  username: string,
+  email: string,
+  password: string,
+  confirm: string,
+  errors: IError
+}
+
+export default class Signup extends React.Component<{}, IState> {
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    confirm: '',
+    errors: {
+      username: '',
+      email: '',
+      confirm: ''
+    }
+  }
+
+  handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.name as keyof IState;
+    const value = e.currentTarget.value as string;
+    const data: any = {};
+    data[name] = value;
+    this.setState(data);
+  }
+
+  handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    await this.validate();
+    const { errors } = this.state;
+    if(errors.username === '' && errors.email === '' && errors.confirm === '') {
+      try {
+        const { username, email, password } = this.state;
+        const res = await axios.post('/api/users', { username, email, password });
+        const user = res.data;
+      } catch (e) {
+        this.setState({ errors: { ...this.state.errors, ...e.response.data } });
+      }
+    }
+  }
+
+  validate = async () => {
+    await this.clearErrors();
+    if(this.state.password !== this.state.confirm)
+      await this.setState({ errors: { ...this.state.errors, confirm: 'Password does not match' }})
+  }
+
+  clearErrors = async () => {
+    await this.setState({ errors: { username: '', email: '', confirm: '' }});
+  }
+
+  render() {
+    const { username, email, password, confirm, errors } = this.state;
+
+    return (
+      <Layout>
+        <Card>
+          <Card.Title text="Sign up"/>
+          <Form>
+            <Form.Group error={errors.username}>
+              <label htmlFor="username">Username</label>
+              <input id="username" type="text" name="username" value={username} onChange={this.handleChange}/>
+              { errors.username &&
+                <small>{errors.username}</small>
+              }
+            </Form.Group>
+            <Form.Group error={errors.email}>
+              <label htmlFor="email">E-Mail</label>
+              <input id="email" type="email" name="email" value={email} onChange={this.handleChange}/>
+              { errors.email &&
+                <small>{errors.email}</small>
+              }
+            </Form.Group>
+            <Form.Group>
+              <label htmlFor="password">Password</label>
+              <input id="password" type="password" name="password" value={password} onChange={this.handleChange}/>
+            </Form.Group>
+            <Form.Group error={errors.confirm}>
+              <label htmlFor="confirm">Confirm password</label>
+              <input id="confirm" type="password" name="confirm" value={confirm} onChange={this.handleChange}/>
+              { errors.confirm &&
+                <small>{errors.confirm}</small>
+              }
+            </Form.Group>
+            <Button mode="primary" onClick={this.handleSubmit}>Sign up</Button>
+          </Form>
+        </Card>
+      </Layout>
+    )
+  }
+}
